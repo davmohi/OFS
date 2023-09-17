6c27fa7eb528a78409a47c0263f7a5eff49f05c0
davmohi <davmohi@gmail.com>
2023-09-17 15:45:43 -0600

    compile, eval y parte de guardar y cargar

--- components/EditorArea.tsx
+++ components/EditorArea.tsx
@@ -3,23 +3,58 @@ import React, { useState } from 'react';
 
 const EditorArea: React.FC<{ setTranspiledCode: (code: string) => void }> = ({ setTranspiledCode }) => {
     const [editorContent, setEditorContent] = useState('');
+    const [inputValue, setInputValue] = useState('');
 
-    const handleCompileClick = async () => {
+    const compileCode = async () => {
         try {
-            const response = await fetch('/compile', {
+            const response = await fetch('/api/compile', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({ content: editorContent })
             });
-            const data = await response.json();
-            setTranspiledCode(data.transpiledContent);
+
+            const data = response.ok ? await response.json() : null;
+            //json to string
+            const stringifiedData = data
+            setTranspiledCode(stringifiedData)
+
+            if (!response.ok) {
+                console.error("Error al compilar");
+            }
         } catch (error) {
-            const timestamp = new Date().toISOString();
-            const dummyResponse = `${editorContent}\n\nTimestamp: ${timestamp}`;
-            setTranspiledCode(dummyResponse);
             console.error("Error al compilar", error);
+        }
+    };
+
+    const guardar = async () => {
+        try {
+            await fetch('/api/guardar', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ content: editorContent, inputText: inputValue })
+            });
+        } catch (error) {
+            console.error("Error al guardar", error);
+        }
+    };
+
+    const cargar = async () => {
+        try {
+            const response = await fetch(`/api/cargar`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ inputText: inputValue })
+            });
+            const data = await response.text();
+            setEditorContent(data);
+        } catch (error) {
+            console.error("Error al cargar", error);
         }
     };
 
@@ -27,8 +62,20 @@ const EditorArea: React.FC<{ setTranspiledCode: (code: string) => void }> = ({ s
         <div className="editor-container">
             <div className="editor-header">
                 <h3 className="editor-title">Área de Edición</h3>
-                <button onClick={handleCompileClick} className="compile-button">
-                    <img src="/compile-icon.png" alt="Compile" /> {/* Asegúrate de tener un ícono adecuado */}
+                <input
+                    type="text"
+                    placeholder="Id del codigo"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                />
+                <button onClick={guardar} className="save-button">
+                    <img src="/guardar.svg" alt="Guardar" />
+                </button>
+                <button onClick={cargar} className="load-button">
+                    <img src="/cargar.svg" alt="Cargar" />
+                </button>
+                <button onClick={compileCode} className="compile-button">
+                    <img src="/compile-icon.png" alt="Compile" />
                 </button>
             </div>
             <textarea
---
+++ src/app/api/compile/route.ts
@@ -0,0 +1,18 @@
+// compile/route.ts
+import { NextResponse } from 'next/server';
+import { stringify } from 'querystring';
+
+export const POST = async (req: Request) => {
+  try {
+    const requestData = await req.json()
+    //extract just value fron json
+    const value = requestData.content
+    const timestamp = new Date().toISOString()
+    //join the data with the timestamp
+
+    const responseData = ''+value+'\n'+timestamp
+    return NextResponse.json(responseData, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 })
+  }
+}
