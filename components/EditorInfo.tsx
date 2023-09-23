6902529a14ffd83c0ab330451aee021db11fb783
LuisLopez <lilc1105@gmail.com>
2023-09-23 16:01:54 -0600

    FP EI, TA
    Progracion Funcional y ts en Editor info y traspilation Areas

--- components/EditorInfo.tsx
+++ components/EditorInfo.tsx
@@ -1,36 +1,27 @@
-// components/EditorInfo.tsx
 import React, { useContext } from 'react';
 
-// Creamos un contexto para los datos del editor
-const EditorInfoContext = React.createContext<{
+interface EditorInfoData {
   id: string;
   cursorLine: number;
   totalLines: number;
   totalWords: number;
   isSaved: boolean;
-} | null>(null);
+}
 
-// Componente que muestra la información del editor
+const EditorInfoContext = React.createContext<EditorInfoData | null>(null);
+
 const EditorInfo: React.FC = () => {
-  // Usamos useContext para acceder a los datos del contexto
   const editorInfo = useContext(EditorInfoContext);
 
-  if (!editorInfo) {
-    return null;
-  }
-
-  return (
+  return editorInfo ? (
     <div className="editor-info" style={{ display: 'flex' }}>
-        <p className='editor-info-p'>ID: {editorInfo.id}</p>
-        <p className='editor-info-p'>LíneaCursor: {editorInfo.cursorLine}</p>
-        <p className='editor-info-p'>TotalLíneas: {editorInfo.totalLines}</p>
-        <p className='editor-info-p'>TotalPalabras: {editorInfo.totalWords}</p>
-        <p className='editor-info-p'>Guardado: {editorInfo.isSaved ? 'Sí' : 'No'}</p>
+      <p className='editor-info-p'>ID: {editorInfo.id}</p>
+      <p className='editor-info-p'>LíneaCursor: {editorInfo.cursorLine}</p>
+      <p className='editor-info-p'>TotalLíneas: {editorInfo.totalLines}</p>
+      <p className='editor-info-p'>TotalPalabras: {editorInfo.totalWords}</p>
+      <p className='editor-info-p'>Guardado: {editorInfo.isSaved ? 'Sí' : 'No'}</p>
     </div>
-);
-
-
-
+  ) : null;
 };
 
 export { EditorInfo, EditorInfoContext };
