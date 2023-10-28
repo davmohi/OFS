0184cff4ebb532f907a9157d44e498d9f6dcf842
davmohi <davmohi@gmail.com>
2023-10-28 15:06:55 -0600

    added prolog server

--- src/app/services/compile/crud.ts
+++ src/app/services/compile/crud.ts
@@ -6,19 +6,34 @@ import { join } from 'path';
 
 //"Compiles" the code (for the moment it only returns the same code with the date and saves the compiled code in a .js).
 export const compileData = async (content: string, id: string) => {
-  const timestamp = new Date().toISOString();
   
   const filename = `${id.split(".")[0]}.js`; // Sets the name of the script
   const filePath = join(__dirname, '../../../../../scripts.js', filename);
-  
+
   try {
     await writeFile(filePath, content, 'utf-8');
   } catch (error) {
     console.error('Error al guardar el archivo:', error);
   }
+
+  // Fetch the script content from the SWI-Prolog server
+  const serverURL = `http://localhost:8000/retrieve?filename=${filename}`;
+  let serverContent: String = ''
+  try {
+    const response = await fetch(serverURL);
+    if (response.ok) {
+      const data = await response.json();
+      serverContent  = data.content;
+    } else {
+      console.error('Error fetching from SWI-Prolog server:', await response.text());
+    }
+  } catch (error) {
+    console.error('Fetch error:', error);
+    serverContent  = 'Error al cargar desde Prolog';
+  }
   
   return {
-    content: `//${timestamp}\n${content}`,
+    content: serverContent,
     filename, // includes the filename in the response
   };
 };
