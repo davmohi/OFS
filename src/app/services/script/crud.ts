72629b3ef307803ef6ffd8858d144f6476711b65
Andercr22 <49879537+Andercr22@users.noreply.github.com>
2023-10-29 15:33:37 -0600

    add rename and charge mongo
    mongo implementation rename and charge

--- src/app/services/script/crud.ts
+++ src/app/services/script/crud.ts
@@ -31,6 +31,7 @@ export const createScript = async (name: string, body: string) => {
 
 
 
+
 /*export const createScript = async (name: string, body: string) => {
   const script = await prisma.script.create({
       data: {
@@ -43,7 +44,24 @@ export const createScript = async (name: string, body: string) => {
 
 
 //Retrieves and returns a script by its id
-export const read = async(id: string) =>{
+
+export const read = async (name: string) => {
+  const existingScript = await prisma.script.findUnique({
+    where: { name },
+  });
+
+  if (existingScript) {
+    // Si el documento existe, lo devolvemos
+    return existingScript.body;
+  } else {
+    // Si el documento no existe, lanzamos un error personalizado
+    throw new Error("El documento no se encontró en la base de datos.");
+  }
+}
+
+
+
+/*export const read = async(id: string) =>{
   
   const filePath = join(filesDirectory, id);
   const fileExists = await fs.access(filePath).then(() => true).catch(() => false);
@@ -51,10 +69,38 @@ export const read = async(id: string) =>{
     throw new Error('Archivo no encontrado');
   }
   return await fs.readFile(filePath, 'utf8');
-}
+}*/
 
 //Renames a script
-export const rename = async(oldId: string, newId: string) =>{
+export const rename = async (oldName: string, newName: string) => {
+  const existingScript = await prisma.script.findUnique({
+    where: { name: oldName },
+  });
+
+  if (!existingScript) {
+    throw new Error("El documento con el nombre original no se encontró en la base de datos.");
+  }
+
+  const scriptWithNewName = await prisma.script.findUnique({
+    where: { name: newName },
+  });
+
+  if (scriptWithNewName) {
+    // Maneja el conflicto de nombres según tus requisitos, por ejemplo, lanzando un error.
+    throw new Error("El nuevo nombre ya está en uso en la base de datos.");
+  }
+
+  // Actualiza el nombre del documento existente
+  const updatedScript = await prisma.script.update({
+    where: { name: oldName },
+    data: { name: newName },
+  });
+
+  return updatedScript;
+};
+
+
+/*export const rename = async(oldId: string, newId: string) =>{
     const oldFilePath = join(filesDirectory, oldId);
     const newFilePath = join(filesDirectory, newId);
   
@@ -62,4 +108,4 @@ export const rename = async(oldId: string, newId: string) =>{
     return fileExists
     ? await fs.rename(oldFilePath, newFilePath)
     : Promise.reject('Archivo no encontrado');
-  }
+  }*/
