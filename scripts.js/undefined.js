6dc2621eed35d4e86675b84f2d283bd30ad12530
davmohi <davmohi@gmail.com>
2023-11-01 17:08:54 -0600

    df

--- scripts.js/undefined.js
+++ scripts.js/undefined.js
@@ -1,4 +1,4 @@
 const nats = iterate(0, n => n + 1);
 const even = nats.filter(n => n % 2 === 0);
 const evenLessThanEleven = even.filter(n => n < 11);
-evenLessThanEleven.map(n => console.log(n)).cut(10).toList();
+evenLessThanEleven.map(n => console.log(n)).cut(2).toList();
