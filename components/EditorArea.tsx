57523709717817880030dff57a800c60bce7c887
LuisLopez <lilc1105@gmail.com>
2023-11-23 04:29:49 -0600

    coments

--- components/EditorArea.tsx
+++ components/EditorArea.tsx
@@ -1,3 +1,16 @@
+/* 
+University: Universidad Nacional
+School: Escuela de Informatica
+Course: Paradigmas de Programación EIF 400
+Semester: 2 Year: 2023
+Team 02-1pm 
+Members:
+Luis Lopez Castro id: 402420889
+Anderson Mora Aguero id: 115600170
+David Morales Hidalgo id: 116300616
+Alberto Aguero Herdocia id: 118450651
+Project: OneFlowStream (OFS)
+*/
 //components/EditorArea.tsx
 import React, { useState, useEffect, useMemo, useRef, ChangeEvent, MouseEvent, KeyboardEvent } from 'react';
 import { EditorInfo, EditorInfoContext } from './cee/EditorInfo';
--- src/app/services/script/crud.ts
+++ src/app/services/script/crud.ts
@@ -1,4 +1,16 @@
-import { promises as fs } from 'fs';
+/* 
+University: Universidad Nacional
+School: Escuela de Informatica
+Course: Paradigmas de Programación EIF 400
+Semester: 2 Year: 2023
+Team 02-1pm 
+Members:
+Luis Lopez Castro id: 402420889
+Anderson Mora Aguero id: 115600170
+David Morales Hidalgo id: 116300616
+Alberto Aguero Herdocia id: 118450651
+Project: OneFlowStream (OFS)
+*/
 import { join } from 'path';
 import prisma from "../../../libs/client"
 const filesDirectory = join(process.cwd(), 'scripts');
